fbbf51dc44ef294f6165fd70cb6d3257dc86df02
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2018-01-22 18:53:36 -0400

    Hoisting - fin: mueve detalles de implementacion hacia abajo, utilizando hoisting de js

--- src/07-hoisting-foco-codigo-importante.js
+++ src/07-hoisting-foco-codigo-importante.js
@@ -1,39 +1,52 @@
 let View = {
     render() {
+        /**
+         * Obtiene data desde json endpoint
+         * @type {string}
+         */
         var url = `http://localhost:3000/api/${this.accountType}/${this.accountId}`;
         this._fetchData(url)
-            .then(data => {
-                var previousTransactions = data
-                    .filter(x => {
-                        return x.timestamp < Date.now();
-                    })
-                    .map(x => {
-                        return {
-                            type: x.transactionType,
-                            amount: x.transactionAmount,
-                            timestamp: x.timestamp
-                        };
-                    });
+            // transforma data
+            .then(processData)
+            // render html
+            .then(renderHtml);
 
-                return previousTransactions;
-            })
-            .then(transactions => {
-                var rows = transactions.map(x => {
-                    return `<tr>
-                                <td>${x.type}</td>
-                                <td>${x.amount}</td>
-                            </tr>`;
-                });
-                var html = `
-                <table>
-                    ${rows.join('')}
-                <table>`;
 
-                $('#container').html(html);
-            });
     },
 
     _fetchData(url){
         return Promise.resolve([/** Some data **/])
     }
+}
+
+function processData(data) {
+
+    var previousTransactions = data
+        .filter(x => {
+            return x.timestamp < Date.now();
+        })
+        .map(x => {
+            return {
+                type: x.transactionType,
+                amount: x.transactionAmount,
+                timestamp: x.timestamp
+            };
+        });
+
+    return previousTransactions;
+}
+
+function renderHtml(transactions){
+    var rows = transactions.map(x => {
+        return `<tr>
+                                <td>${x.type}</td>
+                                <td>${x.amount}</td>
+                            </tr>`;
+    });
+    var html = `
+                <table>
+                    ${rows.join('')}
+                <table>`;
+
+    $('#container').html(html);
 }
